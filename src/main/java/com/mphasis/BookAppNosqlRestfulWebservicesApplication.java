5a9de4bcd98059f59f3341d622dfcb9271edf8f2
Suchith Kumar <suchith.1995.kumar@gmail.com>
2019-11-13 10:53:00 +0530

    Project Creation

---
+++ src/main/java/com/mphasis/BookAppNosqlRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mphasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAppNosqlRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAppNosqlRestfulWebservicesApplication.class, args);
+	}
+
+}
